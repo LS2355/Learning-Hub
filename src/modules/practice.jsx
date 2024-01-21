3ede8725e49ad8e81acbe009c641e8f70a96a86a
Luis S <Luissalcedo2355@hotmail.com>
2024-01-20 23:42:23 -0800

    practice section MVP but i actually saved this time

--- src/modules/practice.jsx
+++ src/modules/practice.jsx
@@ -21,8 +21,10 @@ export function Practice ({isDemoAccount}) {
 
   const generatedTableData = []
   
-  //this is an attempt to make updateProgresscolor and give color functions into one
-  function ProgressColor (enteredValue) {
+  
+
+// tailwind weird and i can only get the select box colors to generate like this (for the initial load)
+  function handleProgressColor (enteredValue) {
     console.log(enteredValue)
     if (enteredValue == "done"){
       return "text-white bg-done w-full"
@@ -42,58 +44,28 @@ export function Practice ({isDemoAccount}) {
   }
 
 
+      function updateProgress(e){
+        console.log("updated")
+        /* here we are going to update local storage with problems progress change */
 
-  function updateProgressColor (e) {
 
-    let value = e.target.value
-
-    if (value == "done"){
-      e.target.className = "text-white bg-green-600 w-full"
-    }else if(value == "workingOn"){
-      e.target.className = "text-white bg-yellow-400 w-full"
-    }else if(value == "retry"){
-      e.target.className = "text-white bg-orange-600 w-full"
-    }else if(value == "next"){
-      e.target.className = "text-white bg-blue-300 w-full"
-    }
-  }
+      }
 
   function buildTableRow (dataSet) {
     dataSet.forEach((data, index)=>{
       const {problem, link, leetCodeNumber, difficulty, acceptancePersentage, progress, note}= data
       console.log(problem, link, leetCodeNumber, difficulty, acceptancePersentage, note)
       
-// tailwind weird and i can only get the select box colors to generate like this (for the initial load)
-      function giveColor () {
-        if (progress == "done"){
-          return "text-white bg-done"
-        }
-        else if (progress == "workingOn"){
-          return "text-white bg-workingOn"
-        }
-        else if (progress == "retry"){
-          return "text-white bg-retry"
-        }
-        else if (progress == "next") {
-          return "text-white bg-next"
-        }
-      }
-
-
-      function updateProgress(e){
-        if (isDemoAccount == false){
-          console.log("added to localStorage")
-        }else{
-          console.log("updated on screen")
-          console.log("tajkfdsj", e.target.value)
-        } 
-      }
 
 
       const tdTemplate =( 
             <tr className="text-subtext border-b-2 border-borderColor m-0" id={`table-row-${index}`} key={`table-row-${index}`}>  {/* remember to delete the bottom border so it dosen't look weird */}
               <td className="w-2/12 px-3 py-1 border-r border-borderColor ">
-                <select name="progress" className={ProgressColor(progress)} defaultValue={progress? progress: "placeholder"} onChange={(e) =>{e.target.className = ProgressColor(e.target.value); updateProgress(e)}} > 
+                <select name="progress" className={handleProgressColor(progress)} defaultValue={progress? progress: "placeholder"} onChange={(e) =>{
+                  e.target.className = handleProgressColor(e.target.value);
+                   if(!isDemoAccount)updateProgress()
+                   }} 
+                > 
                   <option value="placeholder" disabled >select progress</option>
                   <option value="done">done</option>                                                                                   
                   <option value="workingOn">working on</option>                                                                               
@@ -102,10 +74,14 @@ export function Practice ({isDemoAccount}) {
                 </select> 
               </td> {/* progress */}
               <td className="w-1/12 px-3 py-1 border-l border-r border-borderColor text-center ">{leetCodeNumber}</td>  {/* leetcode number */}
-              <td className="w-4/12 px-4 py-1 border-l border-r border-borderColor text-center"><a className="leetcode_link" href={link} target="blank_">{problem}</a></td>  {/* problem title  and link to problem*/}
+              <td className="w-4/12 px-4 py-1 border-l border-r border-borderColor text-center">{/* problem title  and link to problem*/}
+                <a className="leetcode_link" href={link} target="blank_">{problem}</a>
+              </td>  
               <td className="w-1/12 px-3 py-1 border-l border-r border-borderColor text-center ">{acceptancePersentage}</td>  {/* acceptance % */}
               <td className="w-1/12 px-3 py-1 border-l border-r border-borderColor text-center ">{difficulty}</td>  {/* difficulty  / set color of text deppending on difficulty this will be sent on generation */}
-              <td className="w-3/12 px-3 py-1 border-l border-borderColor "><input type="text" name="text" className="note-box" placeholder="........." value={note}/></td>  {/* notes */}            
+              <td className="w-3/12 px-3 py-1 border-l border-borderColor ">{/* notes */}      
+                <input type="text" name="text" className="note-box" placeholder="........." value={note}/>
+                </td>        
             </tr>       
     )
     
